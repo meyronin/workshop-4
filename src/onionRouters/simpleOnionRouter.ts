7559ac1ff50c8b79b05f44de98f15a10bcf48ebf
martin liger <martin.liger@edu.devinci.fr>
2024-03-11 11:02:51 +0100

    adding missing server

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,6 +1,11 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
+
+// Variables to store last received messages and destination
+let lastReceivedEncryptedMessage: string | null = null;
+let lastReceivedDecryptedMessage: string | null = null;
+let lastMessageDestination: number | null = null;
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
@@ -12,14 +17,25 @@ export async function simpleOnionRouter(nodeId: number) {
     res.send("live");
   });
 
+  // GET route to retrieve the last received encrypted message
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req: Request, res: Response) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  // GET route to retrieve the last received decrypted message
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req: Request, res: Response) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  // GET route to retrieve the last message's destination
+  onionRouter.get("/getLastMessageDestination", (req: Request, res: Response) => {
+    res.json({ result: lastMessageDestination });
+  });
+
+  // Start server
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
-    console.log(
-      `Onion router ${nodeId} is listening on port ${
-        BASE_ONION_ROUTER_PORT + nodeId
-      }`
-    );
+    console.log(`Onion router ${nodeId} is listening on port ${BASE_ONION_ROUTER_PORT + nodeId}`);
   });
 
   return server;
 }
-
